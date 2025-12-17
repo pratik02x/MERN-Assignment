66065278d207b827d3c3d34e542b272c0829291e
Pratik <pratikpatil12324@gmail.com>
2025-12-17 18:21:21 +0530

    login form updated with all validation

--- Assignment-4(Login form)/app.js
+++ Assignment-4(Login form)/app.js
@@ -1,3 +1,41 @@
-let email=document.querySelector("#email");
-let pass=document.querySelector("#pass");
+let email=document.getElementById("email");
+let pass=document.getElementById("pass");
+
+const btn=document.getElementById("btn");
+const errore=document.querySelector(".error-msg");
+const errorp=document.querySelector(".error-pass");
+
+
+
+
+function isValid(){
+
+    let isvalidform=true;
+    if(email.value.trim()==="" ){
+        errore.style.display="block";
+        errore.style.color="red";
+        isvalidform=false;
+    }else{
+        errore.style.display="none";
+    }
+
+    
+    
+
+    if(pass.value.trim()===""){
+        errorp.style.display="block";
+        errorp.style.color="red";
+        isvalidform=false
+    } else   {
+        errorp.style.display="none";
+    }
+    
+    
+    if(isvalidform){
+        alert("Login successful..");
+    }
+   
+}
+
+btn.addEventListener("click",isValid);
 
